c0433d33f4cb080aec46904213639e524d29561f
Kyle Welch <1295580+kwelch@users.noreply.github.com>
2023-06-21 22:45:19 -0500

    Make changes for 2023 cards

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 If you really want to use this for your own purposes, here's what you'll need to do.
 
-* Clone / download repository
-* Run `npm install`
-* Replace `/speaker-cards/assets/SpeakerCardBackground.png` with your background image. Recommend 2048x1024.
-* Add speakers to `/src/speaker_data.json`. See the sample file for expected format.
-* Tweak `/speaker-cards/assets/styles.css` and `/src/template.hbs` to meet your needs.
+- Clone / download repository
+- Run `npm install`
+- Replace `/speaker-cards/assets/SpeakerCardBackground.png` with your background image. Recommend 2048x1024.
+- Add speakers to `/src/speakers.json`. See the sample file for expected format.
+- Tweak `/speaker-cards/assets/styles.css` and `/src/template.hbs` to meet your needs.
 
 > Note: You need to install any fonts you need locally. Web fonts do not seem to render properly to image.
 
--- src/index.js
+++ src/index.js
@@ -1,117 +1,129 @@
 "use strict";
 
-const fs = require( "fs-extra" );
-const handlebars = require( "handlebars" );
-const log = require( "./logger" );
-const speakerJson = "./src/speaker_data.json";
-const open = require( "open" );
-const { spawn } = require( "child_process" );
-const hapi = require( "@hapi/hapi" );
-const path = require( "path" );
-const inert = require( "@hapi/inert" );
+const fs = require("fs-extra");
+const handlebars = require("handlebars");
+const log = require("./logger");
+const speakerJson = "./src/speakers.json";
+const open = require("open");
+const hapi = require("@hapi/hapi");
+const path = require("path");
+const inert = require("@hapi/inert");
 
-const getTwitterHandle = twitterLink => {
-  return "@" + twitterLink.replace( "https://twitter.com/", "" );
+const getTwitterHandle = (twitter) => {
+  if (twitter.startsWith("@")) {
+    return twitter;
+  } else {
+    return `@${twitter}`;
+  }
 };
 
-const normalizeSpeakerData = async speakers => {
+const normalizeSpeakerData = async (speakers) => {
   let updated = false;
-  speakers.forEach( speaker => {
-    if ( !speaker.slug ) {
+  speakers.forEach((speaker) => {
+    if (!speaker.slug) {
       updated = true;
-      speaker.slug = slugify( speaker.name );
+      speaker.slug = slugify(speaker.name);
     }
-    if ( !speaker.twitterLink ) {
+    if (!speaker.twitterLink) {
       updated = true;
-      speaker.twitterLink = speaker.twitter;
-      speaker.twitter = getTwitterHandle( speaker.twitter );
+      speaker.twitterLink = `https://twitter.com/${speaker.twitter.replace(
+        "@",
+        ""
+      )}`;
+      speaker.twitter = speaker.twitter
+        ? getTwitterHandle(speaker.twitter)
+        : null;
     }
-  } );
-  if ( updated ) {
-    log.info( "updating speaker file" );
-    await fs.writeJSON( speakerJson, speakers, { spaces: 2 } );
+  });
+  if (updated) {
+    log.info("updating speaker file");
+    await fs.writeJSON(speakerJson, speakers, { spaces: 2 });
   }
   return speakers;
 };
 
-const slugify = name => {
-  return name.toLowerCase().replace( /[ .]/g, "_" );
+const slugify = (name) => {
+  return name.toLowerCase().replace(/[ .]/g, "_");
 };
 
-const generateTweets = async speakers => {
-  fs.ensureDir( "./tweets" );
-  speakers.forEach( async speaker => {
-    const slug = slugify( speaker.name );
-    log.info( `generating tweets for ${ slug }` );
-    const tweet = `Come hear ${ speaker.twitter } at @undergroundjs present "${ speaker.bio }"
+const generateTweets = async (speakers) => {
+  fs.ensureDir("./tweets");
+  speakers.forEach(async (speaker) => {
+    const slug = slugify(speaker.name);
+    log.info(`generating tweets for ${slug}`);
+    const tweet = `Come hear ${
+      speaker.twitter || speaker.name
+    } at @undergroundjs present "${speaker.title}"
   
-Excited to have ${ speaker.twitter } at @undergroundjs present "${ speaker.bio }"`;
-    await fs.writeFile( `./tweets/${ slug }.txt`, tweet );
-  } );	  
+Excited to have ${speaker.twitter || speaker.name} at @undergroundjs present "${
+      speaker.title
+    }"`;
+    await fs.writeFile(`./tweets/${slug}.txt`, tweet);
+  });
 };
 
-const renderSpeakerCards = speakers => {
-  speakers.forEach( speaker => {
-    const url = `http://localhost:8080/${ speaker.slug }.html`;
-    log.info( `rendering ${ url }` );
-    open( url );
-  } );
+const renderSpeakerCards = (speakers) => {
+  speakers.forEach((speaker) => {
+    const url = `http://localhost:8080/${speaker.slug}.html`;
+    log.info(`rendering ${url}`);
+    open(url);
+  });
 };
 
-const buildSpeakerHtmlCards = async speakers => {
-  fs.ensureDir( "./speaker-cards" );
-  const templateFile = await fs.readFile( "./src/template.hbs", "utf8" );
-  const template = handlebars.compile( templateFile );
-  speakers.forEach( async speaker => {
-    const html = template( speaker );
-    const fileName = `./speaker-cards/${ speaker.slug }.html`;
-    log.info( `creating ${ fileName }` );
-    await fs.writeFile( fileName, html );
-  } );
+const buildSpeakerHtmlCards = async (speakers) => {
+  fs.ensureDir("./speaker-cards");
+  const templateFile = await fs.readFile("./src/template.hbs", "utf8");
+  const template = handlebars.compile(templateFile);
+  speakers.forEach(async (speaker) => {
+    const html = template(speaker);
+    const fileName = `./speaker-cards/${speaker.slug}.html`;
+    log.info(`creating ${fileName}`);
+    await fs.writeFile(fileName, html);
+  });
 };
 
 const launchServer = async () => {
   try {
-    const speakerPath = path.resolve( __dirname, "..", "speaker-cards" );
-    const server = hapi.server( {
+    const speakerPath = path.resolve(__dirname, "..", "speaker-cards");
+    const server = hapi.server({
       port: 8080,
       routes: {
         files: {
-          relativeTo: speakerPath
-        }
-      }
-    } );
-    await server.register( inert );
-    server.route( {
+          relativeTo: speakerPath,
+        },
+      },
+    });
+    await server.register(inert);
+    server.route({
       method: "GET",
       path: "/{param*}",
       handler: {
         directory: {
           path: ".",
-          redirectToSlash: true
-        }
-      }
-    } );
+          redirectToSlash: true,
+        },
+      },
+    });
     await server.start();
-    log.info( `server running at: ${ server.info.uri } pointed to ${ speakerPath }` );
+    log.info(`server running at: ${server.info.uri} pointed to ${speakerPath}`);
     return server;
-  } catch ( err ) {
-    log.error( `launchServer error: ${ err.message }` );
+  } catch (err) {
+    log.error(`launchServer error: ${err.message}`);
   }
 };
 
 const main = async () => {
-  const fileExists = await fs.exists( speakerJson );
-  if ( !fileExists ){
-    log.error( "The speaker_data.json file is missing!" );
+  const fileExists = await fs.exists(speakerJson);
+  if (!fileExists) {
+    log.error("The speaker_data.json file is missing!");
     return;
   }
-  const rawSpeakers = await fs.readJSON( speakerJson );
-  const speakers = await normalizeSpeakerData( rawSpeakers );
-  await generateTweets( speakers );
-  await buildSpeakerHtmlCards( speakers );
+  const rawSpeakers = await fs.readJSON(speakerJson);
+  const speakers = await normalizeSpeakerData(rawSpeakers);
+  await generateTweets(speakers);
+  await buildSpeakerHtmlCards(speakers);
   await launchServer();
-  renderSpeakerCards( speakers );
+  renderSpeakerCards(speakers);
   // setTimeout( () => {
   //   server.stop();
   // }, 2000 );
